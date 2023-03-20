cebfeedaa0317becfa1e48d8090f7658ccfe1a70
lowfitoa <xnick411@dicolabs.kr>
2023-03-20 21:43:09 +0900

    feat: 🎸 add proto

---
+++ index.js
@@ -0,0 +1,17 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [
+    GatewayIntentBits.Guilds,
+    GatewayIntentBits.GuildMessages,
+    GatewayIntentBits.MessageContent,
+  ],
+});
+
+client.on("ready", () => {
+  console.log("Bot is online!");
+});
+
+console.log(process.env.BOT_TOKEN, "console");
+
+client.login(process.env.BOT_TOKEN);
