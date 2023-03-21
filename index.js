f696429870e9f2af5f0683d6f03cb51601604718
lowfitoa <xnick411@dicolabs.kr>
2023-03-22 00:57:10 +0900

    fix: 🐛 add axios, add agent calls

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { Client, GatewayIntentBits } = require("discord.js");
 
+const axios = require("axios");
+
 require("dotenv").config();
 
 const client = new Client({
@@ -10,13 +12,28 @@ const client = new Client({
   ],
 });
 
-client.on("messageCreate", (msg) => {
+const getAgents = async () => {
+  const url = "https://valorant-api.com/v1/agents";
+  try {
+    const data = axios.get(url).then(async (data) => await data.data.data);
+    return data;
+  } catch (err) {
+    console.error(err, "agent err");
+  }
+};
+
+client.on("messageCreate", async (msg) => {
   if (msg.content === "ping") {
     msg.reply("pong");
   }
+
+  if (msg.content === "agents") {
+    const agents = await getAgents();
+    await msg.reply(`${agents.map((agent) => agent.displayName).join(", ")}`);
+  }
 });
 
-client.on("ready", () => {
+client.on("ready", async () => {
   console.log("Bot is online!");
 });
 
